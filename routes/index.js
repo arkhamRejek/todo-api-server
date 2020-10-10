f00fc0feadd14fd07676d20f4d76e5113fc4c689
Paul-E <autonomous-deus@Paul-Es-MBP.attlocal.net>
2020-10-10 18:55:19 -0400

    update server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-import { todoRoutes } from "./todo";
 import { Router } from "express";
+
+import { todoRoutes } from "./todo";
+import { userRoutes } from "./user";
 
 const routes = Router();
 
 routes.use(todoRoutes);
+routes.use(userRoutes);
 
 export { routes };
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,47 @@
-import { Router } from "express";
 import db from "../models";
-
+import { Router } from "express";
 const userRoutes = Router();
+userRoutes.get("/api/users", (req, res) => {
+  return db.User.findAll()
+    .then((users) => res.send(users))
+    .catch((err) => {
+      console.log("error occured", JSON.stringify(err));
+      return res.send(err);
+    });
+});
+userRoutes.post("/api/users", (req, res) => {
+  const { firstName, lastName, age, color } = req.body;
+  return db.User.create({ firstName, lastName, age, color })
+    .then((user) => res.send(user))
+    .catch((err) => {
+      console.log("error happened on creation", JSON.stringify(err));
+      return res.status(400).send(err);
+    });
+});
+userRoutes.put("/api/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { first_name, last_name, age } = req.body;
+  const payload = {
+    first_name,
+    last_name,
+    age,
+  };
+  return db.user
+    .update(payload, {
+      where: { id },
+    })
+    .then((user) => res.send(user))
+    .catch((err) => res.send(err));
+});
 
-
-
-userRoutes.post("/api/users/register", (req, res) => {
-    const { first_name, last_name, email, password,  age } = req.body;
-
-    const encrypted_password = password
-  
-    return db.todo
-      .create({ first_name, last_name, email, age, encrypted_password})
-      .then(() => res.send(todo))
-      .catch((err) => res.send(err));
-  });
+userRoutes.delete("/api/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  return db.User.destroy({
+    where: { id },
+  })
+    .then((User) => res.send("User has been Deleted!"))
+    .catch((err) => {
+      console.log("error happened on creation", JSON.stringify(err));
+    });
+});
+export { userRoutes };
